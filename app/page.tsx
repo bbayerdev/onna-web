b3d7b55d8d0808e152fdd417c6a61c5a9ce0ffee
bayer <bbayerdev@gmail.com>
2024-07-21 11:26:50 -0300

    landing 1.2

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,25 @@
 
-import { opens } from '../app/layout'
 import Header from './components/header';
-import Logo from './components/logo';
-import Quadrinho from './components/quadrinho';
+import RoundedButom from './components/roundedButom';
+import Section1 from './components/section1';
+import Section2 from './components/section2';
+import Section3 from './components/section3';
 
 export default function Home() {
   return (
     <main className='antialiased'>
       <Header />
-      <section className='flex flex-row justify-center py-16 px-12 gap-32'>
-        <div className='text-left mt-24 space-y-7 w-max max-w-lg'>
-          <h1 className='font-bold text-red-900 text-7xl'>Bem-vinda ao ONNA</h1>
-          <p className={`font-opensans text-2xl`}>
-            Sua Plataforma de Saúde Feminina e Comunidade de Apoio!
-          </p>
-          <p className={`font-opensans text-2xl`}>
-            Cuidado personalizado para todas as fases da saúde feminina!
-          </p>
-          <button className='transition duration-500 hover:bg-red-100 hover:scale-105 shadow-xl font-bold text-3xl text-red-900 border-red-900 border-[3px] rounded-full px-12 py-1'>
-            Explore
-          </button>
-        </div>
-        <Quadrinho />
-      </section>
+      <Section1 />
+      
+      <div className='space-y-64'>
+        <Section2 />
+        <Section3/>
+        
+        <section>
+ 
+        </section>
+      </div>
+
     </main>
   );
 }
