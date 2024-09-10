4fbdd05ee641cada06800758858a90cc8392eb5c
bbayerdev <bbayerdev@gmail.com>
2024-09-10 11:03:21 -0300

    page sobre 1.3

--- app/sobre/components/sectionMetas.tsx
+++ app/sobre/components/sectionMetas.tsx
@@ -1,29 +1,38 @@
 
-import CardMetas from '@/app/sobre/components/cardMetas'
 import React from 'react'
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+} from "@/components/ui/accordion"
 
 
 const SectionMetas = () => {
     return (
-        <section className='flex flex-col w-4/5 justify-center space-y-12 m-auto mt-24'>
-            <h1 className='text-7xl text-red-900 font-bold text-center'>Metas</h1>
+        <section className='flex justify-center flex-col m-auto w-2/3'>
+            <h1 className='text-7xl text-red-900 font-bold text-left'>Metas</h1>
 
-            <div className='w-full h-[320px] border-black flex flex-row space-x-12 justify-center'>
-                <CardMetas
-                    titulo='Aumentar o engajamento do usuário'
-                    meta='Aumentar o número de usuárias ativas em nossa plataforma'
-                />
-
-                <CardMetas
-                    titulo='Melhorar a acessibilidade'
-                    meta='Plataforma mais acessível e fácil de usar para mulheres de todas as origens e níveis de experiência'
-                />
-
-                <CardMetas
-                    titulo='Atualizar regularmente os conteúdos'
-                    meta='Garantindo que nossas usuárias tenham acesso a conteúdos precisos e úteis em sua jornada de cuidados com a saúde'
-                />
-            </div>
+            <Accordion type="single" collapsible className="w-full pt-16 space-y-10">
+                <AccordionItem value="item-1">
+                    <AccordionTrigger className='text-3xl'>Aumentar o engajamento do usuário</AccordionTrigger>
+                    <AccordionContent className='text-2xl'>
+                        Aumentar o número de usuárias ativas em nossa plataforma
+                    </AccordionContent>
+                </AccordionItem>
+                <AccordionItem value="item-2">
+                    <AccordionTrigger className='text-3xl'>Melhorar a acessibilidade </AccordionTrigger>
+                    <AccordionContent className='text-2xl'>
+                        Plataforma mais acessível e fácil de usar para mulheres de todas as origens e níveis de experiência
+                    </AccordionContent>
+                </AccordionItem>
+                <AccordionItem value="item-3">
+                    <AccordionTrigger className='text-3xl'>Atualizar regularmente os conteúdos</AccordionTrigger>
+                    <AccordionContent className='text-2xl'>
+                        Garantindo que nossas usuárias tenham acesso a conteúdos precisos e úteis em sua jornada de cuidados com a saúde
+                    </AccordionContent>
+                </AccordionItem>
+            </Accordion>
 
         </section>
     )
