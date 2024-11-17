459b71c341badbda79cc58bcda1fa8611cab0829
bayer <bbayerdev@gmail.com>
2024-11-16 23:21:42 -0300

    page artigos (front)

---
+++ app/comunidade/artigos/components/CardArtigo.tsx
@@ -0,0 +1,78 @@
+import React from 'react'
+import { Baby, Droplets, Heart, Ribbon, Search, Sprout } from 'lucide-react'
+import {
+    Card,
+    CardContent,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import Link from 'next/link'
+import { Button } from '@/components/ui/button'
+
+
+type Props = {
+    id: number,
+    idArtigo: number,
+    titulo: string,
+    body: string,
+    data: string
+}
+
+const CardArtigo = ({ body, data, idArtigo, id, titulo }: Props) => {
+    return (
+        <Card>
+            <div className='hover:bg-zinc-100 transition-transform'>
+                <CardHeader>
+                    <div className="flex flex-row justify-between">
+                        {
+                            idArtigo === 1 ? (
+                                <div className="bg-red-200 rounded-lg p-1">
+                                    <Droplets className="size-8" color="#ef4444" />
+                                </div>
+                            ) : idArtigo === 2 ? (
+                                <div className="bg-blue-50 rounded-lg p-1">
+                                    <Baby className="size-8" color="#60a5fa" />
+                                </div>
+                            ) : idArtigo === 3 ? (
+                                <div className="bg-green-100 rounded-lg p-1">
+                                    <Sprout className="size-8" color="#4ade80" />
+                                </div>
+                            ) : idArtigo === 4 ? (
+                                <div className="bg-zinc-100 rounded-lg p-1">
+                                    <Heart className="size-8" color="#ef4444" />
+                                </div>
+                            ) : (
+                                <div className="bg-yellow-50 rounded-lg p-1">
+                                    <Ribbon className="size-8" color="#fde047" />
+                                </div>
+                            )
+                        }
+                        <h1 className="text-zinc-500 font-bold mt-2">• {data}</h1>
+                    </div>
+                    <div>
+                        <CardTitle className="text-xl mt-2">
+                            {titulo}
+                        </CardTitle>
+                    </div>
+                </CardHeader>
+                <CardContent>
+                    <p>
+                        {body.length > 120 ? `${body.slice(0, 120)}...` : body}
+                    </p>
+                </CardContent>
+                <CardFooter className="flex justify-end">
+                    <Link href={'#'}>
+                        <Button className="mr-1 hover:bg-blue-100" variant="outline" size="icon">
+                            <Search color="#2563eb" className="h-4 w-4" />
+                        </Button>
+                    </Link>
+                </CardFooter>
+            </div>
+        </Card>
+
+
+    )
+}
+
+export default CardArtigo
--- app/comunidade/artigos/page.tsx
+++ app/comunidade/artigos/page.tsx
@@ -1,8 +1,74 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
+import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group"
+import { Baby, Droplets, Heart, Ribbon, Search, Sprout } from 'lucide-react'
+import CardArtigo from './components/CardArtigo'
+
 
 const page = () => {
+
+  const [id, setId] = useState(1)
+
   return (
-    <div>ARTGIOS BITCH</div>
+    <main>
+      <section className='flex flex-row gap-4'>
+        <div className='flex gap-4 mt-1'>
+          <h1 className='text-xl font-bold'>Artigos</h1> <h1 className='text-xl'>•</h1>
+        </div>
+        <div>
+          <ToggleGroup type="single" defaultValue='1' onValueChange={(value) => (setId(parseInt(value)))}>
+            <ToggleGroupItem variant={'outline'} value="1"> <Droplets /> Menstruação</ToggleGroupItem>
+            <ToggleGroupItem variant={'outline'} value="2"> <Baby /> Engravidar</ToggleGroupItem>
+            <ToggleGroupItem variant={'outline'} value="3"> <Sprout /> Gestação</ToggleGroupItem>
+            <ToggleGroupItem variant={'outline'} value="4"> <Heart /> Maternidade</ToggleGroupItem>
+            <ToggleGroupItem variant={'outline'} value="5"> <Ribbon /> Autocuidado</ToggleGroupItem>
+          </ToggleGroup>
+        </div>
+      </section>
+
+      <section className='grid grid-cols-4 gap-10 mt-10'>
+
+        <CardArtigo
+          id={1}
+          idArtigo={1}
+          titulo="Aspectos atuais do diagnóstico e tratamento da endometriose"
+          body="Essa é uma pergunta que ninguém pode responder por você, a não ser você mesma. A decisão de trazer outro ser humano para este mundo não é fácil..."
+          data="15/11/2024"
+        />
+
+        <CardArtigo
+          id={1}
+          idArtigo={2}
+          titulo="Aspectos atuais do diagnóstico e tratamento da endometriose"
+          body="Essa é uma pergunta que ninguém pode responder por você, a não ser você mesma. A decisão de trazer outro ser humano para este mundo não é fácil..."
+          data="15/11/2024"
+        />
+
+        <CardArtigo
+          id={1}
+          idArtigo={3}
+          titulo="Aspectos atuais do diagnóstico e tratamento da endometriose"
+          body="Essa é uma pergunta que ninguém pode responder por você, a não ser você mesma. A decisão de trazer outro ser humano para este mundo não é fácil..."
+          data="15/11/2024"
+        />
+        <CardArtigo
+          id={1}
+          idArtigo={4}
+          titulo="Aspectos atuais do diagnóstico e tratamento da endometriose"
+          body="Essa é uma pergunta que ninguém pode responder por você, a não ser você mesma. A decisão de trazer outro ser humano para este mundo não é fácil..."
+          data="15/11/2024"
+        />
+        <CardArtigo
+          id={1}
+          idArtigo={5}
+          titulo="Aspectos atuais do diagnóstico e tratamento da endometriose"
+          body="Essa é uma pergunta que ninguém pode responder por você, a não ser você mesma. A decisão de trazer outro ser humano para este mundo não é fácil..."
+          data="15/11/2024"
+        />
+
+
+      </section>
+    </main>
   )
 }
 
