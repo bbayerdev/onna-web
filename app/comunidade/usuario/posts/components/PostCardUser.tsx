82e0d5feb2dd60851dea2e47b3159311a6b99f38
bayer <bbayerdev@gmail.com>
2024-11-17 21:21:34 -0300

    resposta posts 1.0 (front)

--- app/comunidade/usuario/posts/components/PostCardUser.tsx
+++ app/comunidade/usuario/posts/components/PostCardUser.tsx
@@ -1,6 +1,5 @@
 'use client'
-import { cn } from "@/lib/utils";
-import { Heart, Trash2 } from 'lucide-react';
+import { CornerDownRight, Ellipsis, EllipsisVertical, Heart, Send, SendHorizontal, Trash2 } from 'lucide-react';
 import { Button } from "@/components/ui/button";
 import { useEffect, useState } from "react";
 import { Badge } from "@/components/ui/badge";
@@ -19,6 +18,33 @@ import {
 import { Toaster } from "@/components/ui/toaster"
 import { useRouter } from "next/navigation";
 import { toast } from "@/hooks/use-toast";
+import {
+    Collapsible,
+    CollapsibleContent,
+    CollapsibleTrigger,
+} from "@/components/ui/collapsible"
+import React from "react";
+import { Input } from "@/components/ui/input";
+import { Separator } from '@radix-ui/react-separator';
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import RespostaCard from './RespostaCard';
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+//tipagem resposta zodddd
+const newResposta = z.object({
+    mensagem: z.string().nonempty('Digite sua resposta')
+});
+//inteligencia do ts:
+type newRespostaData = z.infer<typeof newResposta>;
 
 const PostCardUser =
     ({
@@ -40,7 +66,6 @@ const PostCardUser =
         hora: string
         data: string
     }) => {
-
         const [forum, setForum] = useState('')
         useEffect(() => {
             const forumMap: Record<number, string> = {
@@ -50,7 +75,6 @@ const PostCardUser =
             }
             setForum(forumMap[idForum] || '#Autocuidado')
         }, [])
-
         //puxa o nome do user do local storage
         const [dadosUsuario, setDadosUsuario] = useState<{
             nome: string;
@@ -66,15 +90,14 @@ const PostCardUser =
             const nome = dadosUsuario?.nome
         }, [])
         //limpar o localStorage --> localStorage.clear();
-
         const [error, setError] = useState<boolean>(false); // Estado para mensagens de erro
-
+        //async excluir
         async function excluirPost(id: number) {
             try {
                 const response = await axios.put('http://localhost:3000/api/postagemE', {
                     idPostagem: id
                 });
-        
+
                 if (response.status === 200) {
                     toast({
                         title: "Post excluído com sucesso.",
@@ -82,7 +105,7 @@ const PostCardUser =
                         duration: 2000,
                     });
 
-                    setTimeout(() =>{
+                    setTimeout(() => {
                         window.location.reload();
                     }, 200)
                 }
@@ -91,68 +114,138 @@ const PostCardUser =
             }
         }
 
+        //aqui para baixo tudo sobre enviar resposta
+        const [resposta, setResposta] = useState('')
+        const maxResposta = 100
+        const [isOpen, setIsOpen] = React.useState(false)
+
+        // config do useForm com validação Zod
+        const { register, handleSubmit, formState: { errors } } = useForm<newRespostaData>({
+            resolver: zodResolver(newResposta),
+        });
+        const onSubmit = (data: newRespostaData) => {
+            console.log("Resposta enviada:", data);
+        };
+
         return (
-            <figure
-                className={cn(
-                    "relative cursor-pointer overflow-hidden rounded-xl border p-4",
-                    "border-gray-950/[.1] bg-gray-950/[.01] hover:bg-gray-950/[.05] shadow",
-                    'w-full'
-                )}
-            >
-                <div className="flex flex-row items-center gap-4">
-                    <img className="rounded-full size-12" alt="" src='/imgs/cachorra.png' />
-                    <div className="flex flex-row w-full">
-                        <div className='w-full flex gap-2'>
-                            <figcaption className="font-bold text-xl">
-                                {dadosUsuario?.nome.split(" ").slice(0, 2).join(" ") || 'nao logado'}
-                            </figcaption>
-                            <div className='w-1/3'>
-                                <Badge className='pointer-events-none rounded-2xl mt-1'>{forum}</Badge>
+            <Collapsible open={isOpen} onOpenChange={setIsOpen} className="flex w-full flex-col space-y-2">
+
+                <figure className="relative cursor-pointer overflow-hidden rounded-3xl border p-4 border-gray-950/[.1] bg-gray-950/[.01] hover:bg-gray-950/[.05] shadow w-full">
+                    <div className="flex flex-row items-center gap-4">
+                        <img className="rounded-full size-12" alt="" src='/imgs/cachorra.png' />
+                        <div className="flex flex-row w-full">
+                            <div className='w-full flex gap-2'>
+                                <figcaption className="font-bold text-xl">
+                                    {dadosUsuario?.nome.split(" ").slice(0, 2).join(" ") || 'nao logado'}
+                                </figcaption>
+                                <div className='w-1/3'>
+                                    <Badge className='pointer-events-none rounded-2xl mt-1'>{forum}</Badge>
+                                </div>
+                            </div>
+                            <div className='flex justify-end w-full text-sm'>
+                                <p className='px-2'>{data}</p>
+                                <p>•</p>
+                                <p className='px-2 font-bold'> {hora}</p>
+                                <DropdownMenu>
+                                    <DropdownMenuTrigger>
+                                        <Button className=' hover:bg-zinc-200 rounded-full' variant="secondary" size="icon">
+                                            <EllipsisVertical className="h-5 w-5" />
+                                        </Button></DropdownMenuTrigger>
+                                    <DropdownMenuContent side='right'>
+                                        <DropdownMenuItem className='text-base'> Denunciar Resposta</DropdownMenuItem>
+                                    </DropdownMenuContent>
+                                </DropdownMenu>
                             </div>
                         </div>
-                        <div className='flex justify-end w-full text-sm'>
-                            <p className='px-2'>{data}</p>
-                            <p>•</p>
-                            <p className='px-2 font-bold'> {hora}</p>
+                    </div>
+                    <blockquote className="px-10 mt-5 flex flex-col gap-2 justify-center">
+
+                        <h1 className='text-xl mt-2 font-semibold'>{titulo}</h1>
+                        <p className='mt-2 text-justify'>{subtitulo}</p>
+
+                    </blockquote>
+                    <div className='flex justify-between mt-5'>
+
+                        <div>
+                            <CollapsibleTrigger asChild>
+                                <Button variant="outline" className="ml-2">
+                                    <CornerDownRight /> Respostas
+                                </Button>
+                            </CollapsibleTrigger>
+                        </div>
+
+                        <div className='flex gap-2 justify-center items-center'>
+                            <p className='text-sm text-right font-bold'> {reacoes} </p>
+                            <Heart color="#ef4444" fill='#ef4444' className="h-4 w-4 " />
+                            <div className='flex gap-1 mr-2 '>
+                                <AlertDialog>
+                                    <AlertDialogTrigger asChild>
+                                        <Button className='mr-1 hover:bg-red-100 rounded-full' variant="outline" size="icon">
+                                            <Trash2 className="h-5 w-5" color="#ef4444" />
+                                        </Button>
+                                    </AlertDialogTrigger>
+                                    <AlertDialogContent>
+                                        <AlertDialogHeader>
+                                            <AlertDialogTitle className="text-2xl">Excluir este post?</AlertDialogTitle>
+                                            <AlertDialogDescription className="text-base">
+                                                O post será removido e não poderá ser recuperado.
+                                            </AlertDialogDescription>
+                                        </AlertDialogHeader>
+                                        <AlertDialogFooter>
+                                            <AlertDialogCancel>Cancel</AlertDialogCancel>
+                                            <AlertDialogAction onClick={() => excluirPost(id)} className="bg-red-500 hover:bg-red-600"> Confirmar </AlertDialogAction>
+                                        </AlertDialogFooter>
+                                    </AlertDialogContent>
+                                </AlertDialog>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <blockquote className="px-10 mt-5 flex flex-col gap-2 justify-center">
+                    <Toaster />
+                </figure>
 
-                    <h1 className='text-xl mt-2 font-semibold'>{titulo}</h1>
-                    <p className='mt-2 text-justify'>{subtitulo}</p>
-    
-                </blockquote>
-                <div className='flex justify-end mt-5'>
-                    <div className='flex gap-2 justify-center items-center'>
-                        <p className='text-sm text-right font-bold'> {reacoes} </p>
-                        <Heart color="#ef4444" fill='#ef4444' className="h-4 w-4 " />
-                        <div className='flex gap-1 mr-2 '>
-                            <AlertDialog>
-                                <AlertDialogTrigger asChild>
-                                    <Button className='mr-1 hover:bg-red-100' variant="outline" size="icon">
-                                        <Trash2 className="h-5 w-5" color="#ef4444" />
-                                    </Button>
-                                </AlertDialogTrigger>
-                                <AlertDialogContent>
-                                    <AlertDialogHeader>
-                                        <AlertDialogTitle className="text-2xl">Excluir este post?</AlertDialogTitle>
-                                        <AlertDialogDescription className="text-base">
-                                            O post será removido e não poderá ser recuperado.
-                                        </AlertDialogDescription>
-                                    </AlertDialogHeader>
-                                    <AlertDialogFooter>
-                                        <AlertDialogCancel>Cancel</AlertDialogCancel>
-                                        <AlertDialogAction onClick={() => excluirPost(id)} className="bg-red-500 hover:bg-red-600"> Confirmar </AlertDialogAction>
-                                    </AlertDialogFooter>
-                                </AlertDialogContent>
-                            </AlertDialog>
+                <CollapsibleContent className="space-y-2 flex flex-row gap-3">
+                    <aside className='ml-8'>  <Separator className='h-5/6 w-[2px] bg-zinc-200 rounded-lg' orientation="vertical" /> </aside>
+                    <section className='w-full gap-3 flex flex-col'>
+                        <form onSubmit={handleSubmit(onSubmit)} className="flex w-2/3 space-x-2">
+                            <div className="relative flex-1">
+                                <Input
+                                    {...register("mensagem", {
+                                        onChange: (e) => setResposta(e.target.value)
+                                    })}
+                                    maxLength={100}
+                                    placeholder={errors.mensagem?.message || "Digite sua resposta"}
+                                    className={`rounded-xl ${errors.mensagem ? "placeholder-red-500 text-red-500 border-red-500" : ""}`}
+                                />
+                                <p className="text-right text-xs mr-2">
+                                    <span className={resposta.length >= maxResposta ? 'text-red-500' : 'text-black'}>{resposta.length}/{maxResposta}</span>
+                                </p>
+                            </div>
+                            <Button type="submit" size={"icon"} variant={"outline"} className="hover:bg-green-300 rounded-full">
+                                <Send className="size-4" />
+                            </Button>
+                        </form>
 
-                        </div>
-                    </div>
-                </div>
-                <Toaster />
-            </figure>
+                        <RespostaCard
+                            nome='Arthur Martiniano'
+                            data='17/11/2024'
+                            hora='19:11'
+                            likes={12}
+                            mensagem='calaboca jumento'
+                        />
+
+                        <RespostaCard
+                            nome='Bayer '
+                            data='17/11/2024'
+                            hora='19:11'
+                            likes={12}
+                            mensagem='salve xines'
+                        />
+
+
+                    </section>
+
+                </CollapsibleContent>
+            </Collapsible>
         );
     };
 export default PostCardUser
---
+++ app/comunidade/usuario/posts/components/RespostaCard.tsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Button } from '@/components/ui/button'
+import { EllipsisVertical, Heart, TriangleAlert } from 'lucide-react'
+
+type Props = {
+    nome: string
+    data: string
+    hora: string
+    mensagem: string
+    likes: number
+}
+
+const RespostaCard = ({ data, hora, likes, mensagem, nome }: Props) => {
+    return (
+        <figure className="relative overflow-hidden rounded-3xl border p-4 border-gray-950/[.1]  shadow w-4/5">
+            <div className="flex flex-row items-center gap-4">
+                <img className="rounded-full size-8" alt="" src="https://avatar.vercel.sh/jill" />
+                <div className="flex flex-row w-full">
+                    <div className='w-full flex gap-2'>
+                        <figcaption className="font-bold text-base flex gap-2">
+                            {nome}
+                        </figcaption>
+                    </div>
+                    <div className='flex justify-end w-full text-sm'>
+                        <div className='flex flex-row'>
+                            <div className='flex'>
+                                <p className='px-2 text-sm'>{data}</p>
+                                <p>•</p>
+                                <p className='px-2 font-bold'>{hora}</p>
+                            </div>
+
+                            <DropdownMenu>
+                                <DropdownMenuTrigger>
+                                    <Button className=' hover:bg-zinc-200 rounded-full' variant="ghost" size="icon">
+                                        <EllipsisVertical className="h-5 w-5" />
+                                    </Button></DropdownMenuTrigger>
+                                <DropdownMenuContent side='right'>
+                                    <DropdownMenuItem className='text-base'> Denunciar Resposta</DropdownMenuItem>
+                                </DropdownMenuContent>
+                            </DropdownMenu>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <blockquote className="px-10 flex flex-row gap-2 justify-between">
+                <h1 className=' font-semibold'>
+                    {mensagem}
+                </h1>
+            </blockquote>
+            <footer className='justify-end flex '>
+                <div className='flex gap-1 px-2 justify-center items-center'>
+                    <p className='text-sm text-right font-bold'> {likes} </p>
+                    <Button variant={'outline'} size={'icon'} className='rounded-full'> <Heart color="#ef4444" fill='#ef4444' className="h-4 w-4 " /> </Button>
+                </div>
+            </footer>
+        </figure>
+    )
+}
+
+export default RespostaCard
